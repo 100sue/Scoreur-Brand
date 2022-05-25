8173fad17513b1ea7ee6e29a7c85548d660a31a9
appert <scoreur@gmail.com>
2022-05-25 15:26:58 +0200

    changement scroll et menu,ajout arrow

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+// Toggle : permettant le menu deroulant.
+
 function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
     const sideBar = document.querySelector('.sideBar');
     menuToggle.classList.toggle('active')
     sideBar.classList.toggle('active')
 }
+
+// Toggle-Menu : permettant de faire les arrows :
+
+let marker = document.querySelector('#marker');
+let item = document.querySelectorAll('ul li a');
+
+function indicator(e){
+    marker.style.top = e.offsetTop+'px';
+    marker.style.width = e.offsetWidth+'px';
+}
+
+item.forEach(link => {
+    link.addEventListener('mousemove',(e) => {
+        indicator(e.target);
+    })
+})
